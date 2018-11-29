87a8f46bdc25cabd8c503f630200804227b65b6b
panwang.chengpw <chengpan168@126.com>
2018-11-29 15:59:06 +0800

    init

---
+++ src/config/pageConfig.js
@@ -0,0 +1,8 @@
+export default {
+  'navigationBarTitleText': '我要结婚',
+  'enablePullDownRefresh': false,
+  'navigationBarBackgroundColor': '#FFFFFF',
+  'usingComponents': {
+    'i-toast': '/components/iview/toast/index'
+  }
+}
