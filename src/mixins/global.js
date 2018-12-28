16b06938241f8afe0e1dcea87e1a7b98729dbe9b
panwang.chengpw <chengpan168@126.com>
2018-12-28 13:22:00 +0800

    详情

--- src/mixins/global.js
+++ src/mixins/global.js
@@ -2,11 +2,6 @@ import wepy from 'wepy'
 
 export default class globalMixin extends wepy.mixin {
   data = {
-    shareTitles: [
-      '自从有了它，“谁欠我钱，我欠谁钱”再也不糊涂了！',
-      '万万没想到，就这个小程序帮我挽回了好几万损失！',
-      '最近大家都在用这个小工具管理借款记录！'
-    ]
   }
   methods = {
     navTo (path) {
